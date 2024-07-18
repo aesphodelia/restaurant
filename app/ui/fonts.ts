8f281c512c2960ae4fb673058cf14e0870e0af28
Andrei Kuzmin <aesphodelia@gmail.com>
2024-07-18 09:47:23 +0500

    v1

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { League_Script } from "next/font/google";
+import { Marcellus_SC } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const league_script = League_Script({
+    weight:['400'],
+    subsets: ['latin'],
+  });
+  export const marcellus_sc = Marcellus_SC({
+    weight:['400'],
+    subsets: ['latin'],
+  });
+  export const lusitana = Lusitana({
+    weight:['400'],
+    subsets: ['latin'],
+  });
